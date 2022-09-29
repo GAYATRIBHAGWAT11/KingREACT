0166775c2f29483301dc33d73d58ed49322d8ee8
Admin <admin@Admins-MacBook-Air.local>
2022-09-29 15:03:03 +0530

    adding new react projects

---
+++ my-app/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+import AxiosApp from './components/Axios/Axios';
+// import ChangeColour from './components/souvikSir/ChangeColor';
+// import Crud from '/Users/admin/React/my-app/src/components/CRUDapp/Crud.jsx' 
+// import ToDoApp from './components/ToDoApp/ToDoApp';
+// import Todolist from './components/ToDoList/Todolist';
+// import Counter from './components/Buttons/Counter';
+// import Todolist from './components/ToDoList/Todolist';
+// import Cards from './components/Cards/Cards';
+// import { LifeCycle } from './components/LifeCycle';
+// import Counter from './components/UsestateCounter';
+// import { Parent } from './components/Parent';
+// import FavoriteColor from './components/Usestate';
+// import Counter from './components/UsestateCounter';
+// import LoginUi from './components/LoginPage/Login';
+// import { Greeting } from './components/greeting';
+// import Landing from './components/Landing';
+// import Person from './components/Person';
+// import { Names } from './components/Names';
+// import Maincontent from './components/MainContent/Maincontent';
+// import Person from './components/Person';
+// import Navbar from './components/Navbar';
+// import textForm from './components/textForm';
+// import Profile from './components/Profile'
+// import Navbar from './InstaClone/Navbar';
+// import  UseRef  from './components/UseRef/UseRef';
+
+// Toggle=> classname
+ function App(){
+   return(
+     <div>
+       {/* <Landing/> */}
+       {/* <Names/> */}
+       {/* <Greeting/> */}
+       {/* <Person/> */}
+       {/* <LoginUi/> */}
+       {/* <FavoriteColor/> */}
+      {/* <Counter/> */}
+      {/* <Parent/> */}
+      {/* <LifeCycle/> */}
+      {/* <Counter/> */}
+      {/* <Todolist/> */}
+      {/* <Cards/> */}
+      {/* <ToDoApp/> */}
+      {/* <Crud/> */}
+      {/* <ChangeColour/> */}
+      {/* <UseRef/> */}
+      <AxiosApp/>
+     </div>
+   );
+ }
+ export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Cardsjs from './components/Cards/Cards';
+// import Cards from './components/Cards/Cards';
+// import './index.css';
+import App from './App';
+// import Cards from './components/Cards/Cards.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <Cards/> */}
+    {/* <Cards/> */}
+  </React.StrictMode>
+);
